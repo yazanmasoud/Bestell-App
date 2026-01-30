3cbe536f9064f1572430a3cb16c5b5d226e42af6
Yazan Masoud <yazanmas895@gmail.com>
2026-01-30 21:09:13 +0100

    Refactor meal section layout and styling; add meal categories and update CSS for improved spacing

--- scripts/food.js
+++ scripts/food.js
@@ -1,23 +1,77 @@
 let meals = [
    {
+    "category" : "Burger & Sandwiches",
     "name" : "Verggie mashroom black burger",
     "ingredients" : "Mixed green salad, Tomatoes, Edamame, Mushrooms",
     "price" : "16,90"
    },
    {
+    "category" : "Burger & Sandwiches",
     "name" : "All meat burger",
     "ingredients" : "Beef, Bacon, Dill pickles, smoked cheese, Ketchup, BBQ souse",
     "price" : "15,90"
    },
    {
+    "category" : "Burger & Sandwiches",
     "name" : "Beef red burger",
     "ingredients" : "Beef, Cheese, Tomatoes, Lettuce, Onion",
     "price" : "14,90"
    },
    {
+    "category" : "Burger & Sandwiches",
     "name" : "Beef red burger",
     "ingredients" : "Beef, Cheese, Tomatoes, Lettuce, Onion",
     "price" : "14,90"
-   } 
+   },
+   /* pizza */
+   {
+    "category" : "Pizza",
+    "name" : "pizza Margherita",
+    "ingredients" : "Tomato Sauce, Mozzarella",
+    "price" : "11,90"
+   },
+   {
+    "category" : "Pizza",
+    "name" : "Pizza Chorizo",
+    "ingredients" : "Tomato slices, Mozzarella, Chorizo",
+    "price" : "13,90"
+   },
+   {
+    "category" : "Pizza",
+    "name" : "Funghi ",
+    "ingredients" : "Red onion, Olives, Button Mushrooms, Mozzarella",
+    "price" : "12,90"
+   },
+   {
+    "category" : "Pizza",
+    "name" : "Quattro Formaggi with Chicken",
+    "ingredients" : "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+    "price" : "15,90"
+   },
+   /* salad */
+   {
+    "category" : "Salad",
+    "name" : "Warm beef arugula salad",
+    "ingredients" : "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+    "price" : "16,90"
+   },
+   {
+    "category" : "Salad",
+    "name" : "Mini green Salad",
+    "ingredients" : "Green salad, Cucumber, Carrots, Parsley, Radishes",
+    "price" : "7,90"
+   },
+   {
+    "category" : "Salad",
+    "name" : "Green Salad with sea food",
+    "ingredients" : "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    "price" : "16,90"
+   },
+   {
+    "category" : "Salad",
+    "name" : "Vegan green salad with tofu",
+    "ingredients" : " Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+    "price" : "14,90"
+   }
 ]
 
